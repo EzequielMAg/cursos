1d0bfb185e9979fccdb27d97505c0b466b1ca58e
Ezequiel Mamani Aguilar <ezequiel.m.aguilar@gmail.com>
2023-10-23 03:39:53 -0300

    CountriesRoutingModule and LazyLoad - Proyecto countryApp con Angular Curso de Angular 2023, seccion 9 - Fernando Herrera - Devtalles
    Trabajo realizado:
    - CountriesRoutingModule
    - Componentes en file 'pages':
      - by-capital
      - by-country
      - by-region
      - country
    
    - Carga perezosa del modulo 'CountriesModule'
    
    Sin notas. Queda pendiente.

--- devtalles/AngularDeCeroAExperto-Edicion2023-FernandoHerrera/seccion-09-CountrySPA/countryApp/src/app/app-routing.module.ts
+++ devtalles/AngularDeCeroAExperto-Edicion2023-FernandoHerrera/seccion-09-CountrySPA/countryApp/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
 import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     component: HomePageComponent
   },
   {
@@ -14,11 +15,20 @@ const routes: Routes = [
     component: AboutPageComponent
   },
   {
+    path: 'contact',
+    component: ContactPageComponent
+  },
+  {
+    path: 'countries',
+    loadChildren: () => import('./countries/countries.module').then( m => m.CountriesModule )
+  },
+  {
     path: '**',
     //component: HomePageComponent //Hace esto no es lo mismo, va si, nos manda al home, pero no actualiza el url de la pagina,
-    redirectTo: 'home'
+    redirectTo: ''
   },
 ];
+
 
 @NgModule({
   imports: [
