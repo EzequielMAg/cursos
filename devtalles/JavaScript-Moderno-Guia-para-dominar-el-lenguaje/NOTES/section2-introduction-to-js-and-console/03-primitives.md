047c98405812ee53b3cbbcfa0203cd972bbd636b
Ezequiel Mamani Aguilar <ezequiel.m.aguilar@gmail.com>
2026-02-21 06:33:17 -0300

    Repasando primitives de JS

---
+++ devtalles/JavaScript-Moderno-Guia-para-dominar-el-lenguaje/NOTES/section2-introduction-to-js-and-console/03-primitives.md
@@ -0,0 +1,3 @@
+## TIPOS DE DATOS PRIMITIVOS
+
+![alt text](image-3.png)
---
+++ devtalles/JavaScript-Moderno-Guia-para-dominar-el-lenguaje/assets/js/primitivos.js
@@ -0,0 +1,35 @@
+let nombre = "Ezequiel Mamani";
+
+console.log( nombre );
+console.log( typeof nombre );
+
+nombre = 123;
+console.log( typeof nombre );
+
+let esMarvel = false;
+console.log( typeof esMarvel );
+
+let edad = 27;
+console.log( typeof edad );
+
+edad = 27,123;
+console.log( typeof edad );
+
+let superPoder;
+console.log( typeof superPoder );
+
+let soyNull = null;
+console.log( typeof soyNull );
+
+// TIPO DE DATO SYMBOL: principal uso de este tipo de dato es identificar propiedades de manera unica
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+
+console.log( symbol1 );
+console.log( symbol2 );
+
+console.log( typeof symbol1 );
+
+console.log( symbol1 == symbol2);
+console.log( symbol1 === symbol2);
+
