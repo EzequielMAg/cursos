8012cd7aaf4ad3a2048db11ea08ecbc6fb33f6e5
Ezequiel Mamani Aguilar <ezequiel.m.aguilar@gmail.com>
2026-06-13 08:48:17 -0300

    feat: add Spring Boot guide project from DevTalles course

---
+++ devtalles/java-springboot-guia-definitiva/tu-cv-spring-boot/src/main/java/com/devtalles/tu_cv_spring_boot/cv/controller/CvController.java
@@ -0,0 +1,20 @@
+package com.devtalles.tu_cv_spring_boot.cv.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/cv")
+public class CvController {
+
+    @GetMapping("/home")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/pep")
+    public String pep() {
+        return "pep";
+    }
+}
